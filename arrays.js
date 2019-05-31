81a7f0522106171608832d4a24095e657a2acdd5
David Hochbaum <dhochbaum+flatiron@gmail.com>
2019-05-31 18:39:58 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,15 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+function addElementToBeginningOfArray(a, e) {
+  return [e, ...a]
+}
+function destructivelyAddElementToBeginningOfArray(a, e) {
+  a.unshift(e)
+  return a
+}
+function addElementToEndOfArray(a, e) {
+  return [...a, e]
+}
+function destructivelyAddElementToEndOfArray(a, e) {
+  a.psuh(e)
+  return a
+}
